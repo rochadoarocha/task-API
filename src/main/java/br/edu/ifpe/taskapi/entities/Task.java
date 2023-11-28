4ed553c1749b2c46c804d98cb06fdf75dc52d4ba
Ana Clara Fontes <clara.fontes.ac@gmail.com>
2023-11-28 00:55:45 -0300

    task entity

---
+++ src/main/java/br/edu/ifpe/taskapi/entities/Task.java
@@ -0,0 +1,90 @@
+package br.edu.ifpe.taskapi.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="task")
+@Getter
+@Setter
+public class Task {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "title", length = 30, nullable = false)
+	private String title;
+	
+	@Column(name = "description", length = 200, nullable = false)
+	private String description;
+	
+	@Column(name = "status", nullable = false)
+	private boolean status;
+	
+	@ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+	
+	public Task(String title, String description, boolean status, User user) {
+		this.title = title;
+		this.description = description;
+		this.status = status;
+		this.user = user;
+		
+	}
+	
+	public Task() {
+		
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+}

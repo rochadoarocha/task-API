4fd363f8668e319fc734f71d61eeec423604eabb
Matheus Felipe <matheusbarros9107@gmail.com>
2023-12-12 13:47:45 -0300

    remove email validator

--- src/main/java/br/edu/ifpe/taskapi/dto/create/UsersDTO.java
+++ src/main/java/br/edu/ifpe/taskapi/dto/create/UsersDTO.java
@@ -1,9 +1,7 @@
 package br.edu.ifpe.taskapi.dto.create;
 
 import org.springframework.beans.BeanUtils;
-
 import br.edu.ifpe.taskapi.entities.User;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,7 +15,6 @@ public class UsersDTO {
 	private String name;
 	
 	@NotNull(message="Email não pode ser Nulo")
-	@Email(message="Email Inválido")
 	private String email;
 	
 	@NotNull(message="Senha não pode ser Nula")
